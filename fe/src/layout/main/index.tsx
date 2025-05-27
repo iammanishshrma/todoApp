f2fd79f873f659c2e8a7a883f863bfd458d527c3
Manish Sharma <manish.sharma@pelocal.com>
2025-05-27 12:46:21 +0530

    added confirm box

--- fe/src/layout/main/index.tsx
+++ fe/src/layout/main/index.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router";
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
-import { AppSidebar } from "@/components/appSidebar";
+import { AppSidebar } from "@/components/sidebar";
 
 const MainLayout = () => {
     return (
